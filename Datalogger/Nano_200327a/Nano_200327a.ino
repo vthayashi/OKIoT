b2e553b25e3981ee95f872bc8ba8b0435b6a4d29
Victor Hayashi <vtakashi@hotmail.com>
2020-06-03 12:15:36 -0300

    Add files via upload

---
+++ Datalogger/Nano_200327a/Nano_200327a.ino
@@ -0,0 +1,20 @@
+#define Versao "Nano_200327a"
+#define ss_tx_pin 11 // D1mini D7
+#define ss_rx_pin 10 // D1mini D6
+#define ss_speed 9600 // SoftwareSerial
+
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(ss_rx_pin, ss_tx_pin);
+
+void setup() {
+  // put your setup code here, to run once:
+  mySerial.begin(ss_speed); delay(10);
+  mySerial.print("a7");
+
+  Serial.begin(115200); delay(10);
+  Serial.println("Iniciando");
+  Serial.print("log "); Serial.println(Versao);
+}
+
+void loop() {
+}
